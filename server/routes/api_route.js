220711f353c3597f976a41eeaa15e8c5f2c43704
Saksham <-g>
2023-09-27 21:40:25 +0530

    Build Done

--- server/routes/api_route.js
+++ server/routes/api_route.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const apiController = require("../controller/api_controller");
 
 // Welcome Page
-router.get("/", (req, res) => {
-  return res.status(201).send("Hello World");
-});
+// router.get("/", (req, res) => {
+//   return res.status(201).send("Hello World Server");
+// });
 
 //Send Message
 router.post("/send-message", apiController.sendMessage);
